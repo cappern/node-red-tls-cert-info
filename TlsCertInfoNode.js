7c2ae78d61e5e1338f9de5e3bdec4d6f88a6635d
cappern <christoffer@cappelen.org>
2023-04-21 14:26:18 +0200

    Initial commit

---
+++ TlsCertInfoNode.js
@@ -0,0 +1,69 @@
+module.exports = function (RED) {
+    function TlsCertInfoNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        const tls = require("tls");
+        const crypto = require("crypto");
+
+        function toPem(rawCert) {
+            const base64Cert = rawCert.toString("base64");
+            const pem = `-----BEGIN CERTIFICATE-----\n${base64Cert.match(/.{1,64}/g).join("\n")}\n-----END CERTIFICATE-----\n`;
+            return pem;
+        }
+
+        function setStatus(status) {
+            node.status(status);
+        }
+
+        node.on('input', function (msg) {
+            const options = {
+                host: msg.host,
+                servername: msg.host,
+                port: msg.port || 443,
+                rejectUnauthorized: false,
+            };
+
+            const socket = tls.connect(options, () => {
+                
+                // Henter ut sertifikatet til verten
+                var certificate = socket.getPeerCertificate(true);
+                //console.log("Sertifikat hentet:", certificate);
+
+                // Konverterer sertifikatet til PEM-format
+                const pem = toPem(certificate.raw);
+
+                // Legger til informasjon om sertifikatet i msg.cert
+                msg.payload = {
+                    subject: certificate.subject,
+                    keysize: certificate.bits,
+                    serialnumber: certificate.serialNumber,
+                    validFrom: certificate.valid_from,
+                    validTo: certificate.valid_to,
+                    san: certificate.subjectaltname,
+                    pem: pem, // Legger til PEM-formatet av sertifikatet
+                };
+                msg.detailedCertInfo = certificate
+                msg.host = msg.host
+                setStatus({ fill: "green", shape: "dot", text: `Fetched SSL-CERT from ${msg.host}` });
+                node.send(msg);
+
+
+                // Avslutter TCP-tilkoblingen
+                socket.end();
+            });
+
+            socket.on("error", (error) => {
+                console.log("Feil ved TCP-tilkobling:", error);
+                setStatus({ fill: "red", shape: "ring", text: `Error: ${error}` });
+            });
+        });
+
+        node.on('close', function (done) {
+            setStatus({ fill: "red", shape: "ring", text: "disconnected" });
+            done();
+        });
+    }
+
+    RED.nodes.registerType("tls-cert-info", TlsCertInfoNode);
+}
